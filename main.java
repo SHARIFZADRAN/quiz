a5e00508ed18d1e96daf68936111b8f485d87565
SHARIFZADRAN <sharif.z.zadran@gmail.com>
2022-11-02 23:56:05 -0700

    quiz questions...

---
+++ main.java
@@ -0,0 +1,210 @@
+import java.util.Scanner;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		String answer1 = "";
+		String answer2 = "";
+		String answer3 = "";
+		String answer4 = "";
+		String answer5 = "";
+		String answer6 = "";
+		String answer7 = "";
+		String answer8 = "";
+		String answer9 = "";
+		String answer0 = "";
+		Scanner sc = new Scanner(System.in);
+		
+		int numberCorrect = 0;
+		int questions = 4;
+		
+		System.out.println("where does the sun rise From?");
+		System.out.println("a: west");
+		System.out.println("b: east");
+		System.out.println("c: north");
+		System.out.println("d: south");
+		
+		answer1 = sc.nextLine();
+	if (answer1.equals("b"))
+		System.out.println("Good Job - Correct Answer");
+	numberCorrect = numberCorrect + 1;
+	
+	}
+	else
+	 { 
+		System.out.println("Opps - Wrong Answer");
+	}
+	
+			System.out.println("where does the sun rise From?");
+			System.out.println("a: west");
+			System.out.println("b: east");
+			System.out.println("c: north");
+			System.out.println("d: south");
+			
+			answer2 = sc.nextLine();
+		if (answer2.equals("b"));
+			System.out.println("Good Job - Correct Answer");
+		numberCorrect = numberCorrect + 1;
+		
+		}
+		else
+		 { 
+			System.out.println("Opps - Wrong Answer");
+		}
+		
+		
+		System.out.println("where does the sun rise From?");
+		System.out.println("a: west");
+		System.out.println("b: east");
+		System.out.println("c: north");
+		System.out.println("d: south");
+		
+		answer3 = sc.nextLine();
+		if (answer3.equals("b"));
+		System.out.println("Good Job - Correct Answer");
+		numberCorrect = numberCorrect + 1;
+		
+		}
+		else
+		{ 
+		System.out.println("Opps - Wrong Answer");
+		}
+		
+		
+		System.out.println("where does the sun rise From?");
+		System.out.println("a: west");
+		System.out.println("b: east");
+		System.out.println("c: north");
+		System.out.println("d: south");
+		
+		answer4 = sc.nextLine();
+		if (answer4.equals("b"));
+		System.out.println("Good Job - Correct Answer");
+		numberCorrect = numberCorrect + 1;
+		
+		}
+		else
+		{ 
+		System.out.println("Opps - Wrong Answer");
+		}
+		
+		
+		System.out.println("where does the sun rise From?");
+		System.out.println("a: west");
+		System.out.println("b: east");
+		System.out.println("c: north");
+		System.out.println("d: south");
+		
+		answer5 = sc.nextLine();
+		if (answer5.equals("b"));
+		System.out.println("Good Job - Correct Answer");
+		numberCorrect = numberCorrect + 1;
+		
+		}
+		else
+		{ 
+		System.out.println("Opps - Wrong Answer");
+		}
+		
+		
+		System.out.println("where does the sun rise From?");
+		System.out.println("a: west");
+		System.out.println("b: east");
+		System.out.println("c: north");
+		System.out.println("d: south");
+		
+		answer6 = sc.nextLine();
+		if (answer6.equals("b"));
+		System.out.println("Good Job - Correct Answer");
+		numberCorrect = numberCorrect + 1;
+		
+		}
+		else
+		{ 
+		System.out.println("Opps - Wrong Answer");
+		}
+		
+		
+		System.out.println("where does the sun rise From?");
+		System.out.println("a: west");
+		System.out.println("b: east");
+		System.out.println("c: north");
+		System.out.println("d: south");
+		
+		answer7 = sc.nextLine();
+		if (answer7.equals("b"));
+		System.out.println("Good Job - Correct Answer");
+		numberCorrect = numberCorrect + 1;
+		
+		}
+		else
+		{ 
+		System.out.println("Opps - Wrong Answer");
+		}
+		
+		
+		System.out.println("where does the sun rise From?");
+		System.out.println("a: west");
+		System.out.println("b: east");
+		System.out.println("c: north");
+		System.out.println("d: south");
+		
+		answer8 = sc.nextLine();
+		
+		if (answer8.equals("b"));
+		System.out.println("Good Job - Correct Answer");
+		numberCorrect = numberCorrect + 1;
+		
+		}
+		else
+		{ 
+		System.out.println("Opps - Wrong Answer");
+		}
+		
+		
+		System.out.println("where does the sun rise From?");
+		System.out.println("a: west");
+		System.out.println("b: east");
+		System.out.println("c: north");
+		System.out.println("d: south");
+		
+		answer9 = sc.nextLine();
+		
+		if (answer9.equals("b"));
+		System.out.println("Good Job - Correct Answer");
+		numberCorrect = numberCorrect + 1;
+		
+		}
+		else
+		{ 
+		System.out.println("Opps - Wrong Answer");
+		}
+		
+		
+		System.out.println("where does the sun rise From?");
+		System.out.println("a: west");
+		System.out.println("b: east");
+		System.out.println("c: north");
+		System.out.println("d: south");
+		
+		answer10 = sc.nextLine();
+		
+		if (answer10.equals("b"));
+		System.out.println("Good Job - Correct Answer");
+		numberCorrect = numberCorrect + 1;
+		
+		}
+		else
+		{ 
+		System.out.println("Opps - Wrong Answer");
+		}
+		
+		
+		
+			double grade = numberCorrect/10.0 *100.0;
+		}
+}
